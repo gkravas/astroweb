faae894a28f04d791f4874796b3f3d22bc49663e
George Kravas <gkravas@gmail.com>
2017-12-26 11:44:32 +0200

    meta data fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,13 +19,15 @@ export class AppComponent {
     private angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics) {}
 
     ngOnInit() {
-      /*
       const keywords: string = 'Αστρολογία, Αστρολογικές Προβλέψεις, Ημερήσιες Προβλέψεις, Ζώδια, Αστρολογικός Χάρτης';
       const title: string = 'Astro Lucis';
       const description: string = ' Προσωπικές ημερήσιες προβλέψεις δωρεάν!'
       + ' Αστρολογία και Τεχνητή Νοημοσύνη ενώνονται και προσφέρουν αξιόπιστες προβλέψεις.';
       const image: string = 'https://astrolucis.gr/assets/fbImage.png';
+      const site: string = 'https://astrolucis.gr';
       this.metaService.addTags([
+        { name: 'twitter:card', content: "summary" },
+        { name: 'twitter:site', content: "@astrolucis" },
         { name: 'twitter:title', content: title },
         { name: 'twitter:description', content: description },
         { name: 'twitter:image', content: image },
@@ -33,12 +35,12 @@ export class AppComponent {
         { property: 'og:image', content: image },
         { property: 'og:description', content: description },
         { property: 'og:type', content: "website" },
-        { property: 'og:url', content: "https://astrolucis.gr" },
+        { property: 'og:url', content: site },
         { property: 'fb:app_id', content: "1741917992498773" },
         { property: 'og:locale', content: "el_GR" },
         { name: 'keywords', content: keywords },
         { name: 'description', content: description },
-      ]);*/
+      ]);
     }
   getState(outlet) {
     return outlet.activatedRouteData.state;
